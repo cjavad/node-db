6788d3ddc1ea03b290375bc146048bb359a60523
JavadSM <javadshafique@hotmail.com>
2017-10-21 14:58:01 +0200

    Starting work on project

---
+++ app/app.js
@@ -0,0 +1,14 @@
+//Node NoSql database by (c) Javad Shafique
+
+//Require modules that are needed
+const express = require("express");
+const path = require("path");
+const JsonDB = require("node-json-db");
+const auth = require("./lib/auth.js");
+
+//Express config
+const app = express();
+const port = process.env.PORT || 80;
+
+//Database config
+const db = new JsonDB("db", true /*To autosave*/, true /*Save in human reable form*/);
