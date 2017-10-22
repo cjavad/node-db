b6eb09324812746f57ed54359c8e655d58cb7966
JavadSM <javadshafique@hotmail.com>
2017-10-22 09:14:28 +0200

    working

--- app/app.js
+++ app/app.js
@@ -5,17 +5,12 @@ const express = require("express");
 const path = require("path");
 const auth = require("./lib/auth.js");
 const query = require("./lib/query.js"); //database is in there
-//Database config
-const JsonDB = require("node-json-db");
-const db = new JsonDB("db", true /*To autosave*/, true /*Save in human reable form*/);
+//db config
+const db = require("./lib/db.js");
 //Express config
 const app = express();
 const port = process.env.PORT || 8080;
 
-//debuging
-var l = query.parse('db.hello.push({a:"My name is javad"}, true)');
-console.log(l[1][2]);
-
 //Api config
 require("./lib/api.js")(app, query);
-//app.listen(port)
+app.listen(port);
--- app/lib/db.js
+++ app/lib/db.js
@@ -1,1 +1,15 @@
 //manage database and query!
+//Database config
+const JsonDB = require("node-json-db");
+const db = new JsonDB("db", true /*To autosave*/, true /*Save in human reable form*/);
+const query = require("./query.js");
+
+function pquery(q){
+    const out = query.parse(q);
+    var path = out[1][0],
+    command = out[0],
+    args = out[1];
+    console.log("path is", path, "Command is", command, "with args", args);
+    console.log("from", out);
+}
+console.log(pquery('db.hello.com.push({pc: 121}, false)'));
