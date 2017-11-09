f2784421eac73db660b8303b0cea0c918dd96205
JavadSM <javadshafique@hotmail.com>
2017-11-09 17:10:05 +0100

    removed

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,30 +2,6 @@
 //Database config
 const JsonDB = require("node-json-db");
 const db = new JsonDB("db", true /*To autosave*/, true /*Save in human reable form*/);
-const query = require("./query.js");
-
-function pquery(q){
-    const out = query.parse(q);
-    
-    var args = out[1];
-    //Split for array testing
-    var d = args[1].split("[").join().split("]").join().split(",");
- 
-    eval("args[0] = " + args[0])
- 
-    if(args.length === 3){
-        db[out[0]](args[0], args[1], args[2])
-    } else if(args.length === 2){
-        db[out[0]](args[0], args[1])
-    } else if(args.length === 1){
-        db[out[0]](args[0])
-    } else {
-        return false
-    }
-
-    //if success return true
-    return true
-}
 
 
 module.exports = {
