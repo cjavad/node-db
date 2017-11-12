480b61134a75a80f1447e62485300f593dd0d534
JavadSM <javadshafique@hotmail.com>
2017-11-12 10:16:53 +0100

    done with socket lib

---
+++ lib/python/db_socket.py
@@ -0,0 +1,40 @@
+import socket
+import json
+
+def get_obj(username, password, command, path, data = None, override = None):
+    if not command in ["push", "getData", "delete"]:
+        return False
+    elif data and override != None:
+        return {"username":username,"password":password,"command":command,"path":path,"data":data,"override":override}
+    else:
+        return {"username":username,"password":password,"command":command,"path":path}
+
+class database:
+    def __init__(self, host, port, username, password):
+        self.username = username
+        self.password = password
+        self.socket = socket.create_connection((host, port))
+    
+    def get(self, path):
+        obj = bytes(str(get_obj(self.username, self.password, "getData", path)), "utf-8")
+        self.socket.send(obj)
+        data = self.socket.recv(1024).decode("utf-8")
+        try:
+            data = json.loads(data)
+        return 
+
+    def push(self, path, data, override = False):
+        obj = bytes(str(json.dumps(get_obj(self.username, self.password, "push", path, data, override))), "utf-8")
+        self.socket.send(obj)
+        return self.socket.recv(1024).decode("utf-8")
+
+    def get(self, path):
+        obj = bytes(str(get_obj(self.username, self.password, "delete", path)), "utf-8")
+        self.socket.send(obj)
+        return self.socket.recv(1024).decode("utf-8")
+
+    def checkpath(self, path):
+        if self.get(path) == "Path does not exist":
+            return False
+        else:
+            return True
