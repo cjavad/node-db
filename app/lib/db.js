656552f68c7d1782b117d0fe35757b79b3283e8e
Javad Shafique <javad@fatis-mbp.intern>
2017-11-13 08:53:22 +0100

    working on #1 rewriting database

--- app/lib/db.js
+++ app/lib/db.js
@@ -16,12 +16,12 @@ const DATA = ["push", "find"]
  * @constant PATH_ONLY is an a array with all commands that only require a path
  * @constant DATA is an array specifiying commands that also use data
  * @constant ERRORS
- * PROP_ERR is given if not enough properties is given
- * AUTH_ERR is given if a wrong username/password is given
- * PATH_ERR is given if a path does't exist
- * COM_ERR is given if a wrong command is given
- * RUN_ERR is given if a runtime error has occoured
- * PARSE_ERR is given if non valid json input is given
+ * PROP_ERR (0) is given if not enough properties is given
+ * AUTH_ERR (1) is given if a wrong username/password is given
+ * PATH_ERR (2) is given if a path does't exist
+ * COM_ERR (3) is given if a wrong command is given
+ * RUN_ERR (4) is given if a runtime error has occoured
+ * PARSE_ERR (5) is given if non valid json input is given
  */
 
 //Return custom error based on index
@@ -46,52 +46,30 @@ function parse_json(json_str){
     try {
         body = JSON.parse(json_str);
         //if not valid json return false
-    } catch(_){ return error(5); }
+    } catch(_){console.log(_); return error(5); }
     //check if keys exist
-    check1 = (body["path"] === undefined || body["command"] == undefined || body["username"] == undefined || body["password"] === undefined);
-    if(check1){
-        //on or more values if undefined
-        return error(5);
-        //check if command is valid
-    } else if(!VALID_COMMANDS.indexOf(body["command"]) > -1){
-        return error(5);
+    var check1 = (body["path"] === undefined || body["command"] == undefined || body["username"] == undefined || body["password"] === undefined);
+    var check2 = !(VALID_COMMANDS.indexOf(body["command"]) > -1);
+    if(!(check1 && check2)){
+        return body;
     } else {
-        if(body.command == "find"){
-            var over = null;
-        } else {
-            var over = body.override;
-        }
-        json = {
-            username:body.username,
-            password:body.password,
-            command:body.command,
-            path:body.command,
-            data:body.data,
-            override:over
-        }
-        //if it passed all checks return body object
-        return json;
+        return error(5);
     }
 }
 
 
-function use_db(command, path, data, override = false){
-    if(PATH_ONLY.indexOf(command) > -1 && data === undefined && override === null){
+function use_db(body){
+    if(PATH_ONLY.indexOf(body["command"]) > -1){
         try {
-            return db[command](path);
-        } catch (err) {
+            return db[body["command"]](body["path"])
+        } catch(err) {
+            //check if error is a path error or dataerror
             return err.name === "DataError" ? error(2):error(4);
         }
-    } else if (DATA.indexOf(command) > -1 && data !== null){
-        if(command === "find"){
-            //custom stament for query
-            return db[command](data);
-        } else if(command === "push"){
-            return db[command](data, override);
-        } else {
-            return error(3);
-        }
+    } else if(DATA.indexOf(body["command"]) > -1){
+
     } else {
+        //return COM_ERR
         return error(3);
     }
 }
@@ -108,9 +86,10 @@ function parse(jsonstring){
 
 
 
-obj1 = '{"username":"admin","password":"password","command":"push","data":"hello","path":"/path","override":true}'
+obj1 = {username:"admin",password:"password",command:"push",data:"hello",path:"/path",override:true}
 
-obj2 = '{"username":"admin","password":"password","command":"get",path":"/path"}'
+obj2 = {username:"admin",password:"password",command:"getData",path:"/path"}
 
-console.log(parse(obj1))
-console.log(parse(obj2))
+console.log(parse(JSON.stringify(obj1)))
+console.log(parse(JSON.stringify(obj2)))
+console.log(!VALID_COMMANDS.indexOf(obj2["command"]) > -1)
