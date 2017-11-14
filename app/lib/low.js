18b7b6a990bcb0a1af7b80f358ccc516257bec31
Javad Shafique <javad@Fatis-MacBook-Pro.local>
2017-11-14 17:45:58 +0100

    fixing some exports issues

--- app/lib/low.js
+++ app/lib/low.js
@@ -563,11 +563,6 @@ var root = freeGlobal || freeSelf || Function('return this')();
 /** Detect free variable `exports`. */
 var freeExports = typeof exports == 'object' && exports && !exports.nodeType && exports;
 
-/** Detect free variable `module`. */
-var freeModule = freeExports && typeof module == 'object' && module && !module.nodeType && module;
-
-/** Detect the popular CommonJS extension `module.exports`. */
-var moduleExports = freeModule && freeModule.exports === freeExports;
 
 /** Detect free variable `process` from Node.js. */
 var freeProcess = moduleExports && freeGlobal.process;
@@ -17229,30 +17224,5 @@ var runInContext = (function runInContext(context) {
 
 // Export lodash.
 var _ = runInContext();
-
-// Some AMD build optimizers, like r.js, check for condition patterns like:
-if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
-    // Expose Lodash on the global object to prevent errors when Lodash is
-    // loaded by a script tag in the presence of an AMD loader.
-    // See http://requirejs.org/docs/errors.html#mismatch for more details.
-    // Use `_.noConflict` to remove Lodash from the global object.
-    root._ = _;
-
-    // Define as an anonymous module so, through path mapping, it can be
-    // referenced as the "underscore" module.
-    define(function() {
-        return _;
-    });
-}
-// Check for `exports` after `define` in case a build optimizer adds it.
-else if (freeModule) {
-    // Export for Node.js.
-    (freeModule.exports = _)._ = _;
-    // Export for CommonJS support.
-    freeExports._ = _;
-} else {
-    // Export to the global object.
-    root._ = _;
-}
 
 module.exports = _;
