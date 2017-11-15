378bfb1a4de73fa327c23753865a578927908b6f
Javad Shafique <javad@fatis-mbp.intern>
2017-11-15 14:05:39 +0100

    working on iter

--- app/lib/database/lib/Query.js
+++ app/lib/database/lib/Query.js
@@ -3,7 +3,7 @@
 */
 var Eval = require("./eval.js");
 var DataError = require("./Errors").DataError
-var _ = require("../../low");
+var _ = require("lodash");
 
 
 const VALID_OPERATORS = ["===", "==", "!=", "!==", "in", "!", ">", "<", ">=", "<="];
@@ -11,61 +11,41 @@ const VALID_OPERATORS = ["===", "==", "!=", "!==", "in", "!", ">", "<", ">=", "<
 var example_query = {operator:"===", key:"key", value:"tocomparewith"}
 
 class Query {
+  static find(obj, query){
+    var l = [];
+    var i;
+    _.forIn(o, function(value, key) {
+      if(typeof value === "object"){
+        
+        _.forIn(value, function(v, k){
+          console.log(v, k);
+        });
+      } else {
+        l.push([key, value]);
+      }
+    });
+  }
 
-}
-
-function validate_query(query){
-  if(VALID_OPERATORS.indexOf(query.operator) > -1){
-    return query;
-  } else {
-    return false;
+  static zip(){
+    out = [];
+    for (let i = 0; i < arguments.length; i++) {
+      const element = arguments[i];
+        out.push(...element);
+    }
+    return out;
   }
 }
 
-Query.prototype.findKey = function(obj, query, last = false){
-  query = validate_query(query);
-  if(!query){return false;}
-  if(last){
-    return _.findLastKey(obj, function(o){
-      string = ["o", query.operator, query.value].join(" ");
-      return Eval(string); 
-    });
-  } else {
-    return _.findKey(obj, function(o){
-      string = ["o", query.operator, query.value].join(" ");
-      return Eval(string); 
-    });
-  }
-}
-
-
-
-
-Query.prototype.find = function(collection, query, last = false){
-  query = validate_query(query);
-  if(!query){return false;}
-
-  if(last){
-    return _.findLast(collection, function(o){
-      string = ' '.join("o." + query[0], query[1], query[2]);
-      return Eval(string); 
-    });
-  } else {
-    return _.find(collection, function(o){
-      string = ' '.join("o." + query[0], query[1], query[2]);
-      return Eval(string); 
-    });
+o = {
+  this:"is",
+  an:"object",
+  with:{
+    more:"objects",
+    because:true
   }
 }
 
 
-Query.prototype.zip = function(){
-  out = [];
-  for (let i = 0; i < arguments.length; i++) {
-    const element = arguments[i];
-      out.push(...element);
-  }
-  return out;
-}
 
- module.exports = Query;
+
+module.exports = Query;
